805de4afecd05021a63a28381eb1665443e03a9a
Jamie <kookjc6@gmail.com>
2020-03-02 21:44:37 -0600

    added files from develop and routed html routes with precursor to api routes commented out

---
+++ routes/api-routes.js
@@ -0,0 +1,29 @@
+const path = require("path"); 
+module.exports = function(app) {
+
+    //When are API methods being invoked?
+
+    // app.get("/api/workouts", function(req, res) {
+    //     //get all workout data and return json 
+            //when api.getLastWorkout invoked
+    //     res.json()
+    // }); 
+
+    // app.put("/api/workouts/:id", function(req, res) {
+    //     //add a new exercise to an exisiting workout and return json
+            //when API.addExercise invoked
+    //     res.json()
+    // }); 
+
+     // app.post("/api/workouts", function(req, res) {
+    //     //add a new workout to database
+        //when API.createWorkout is invoked
+    //     res.json()
+    // }); 
+
+     // app.get("/api/workouts/range", function(req, res) {
+    //     //????? what is this doing?
+        //when API.getWorkoutsInRange
+    //     res.json()
+    // }); 
+}
